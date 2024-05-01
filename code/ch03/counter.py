8b58ff5b5dc44cdee3816a901f5ab56ef877c143
Yang Ge <eroicaleo@gmail.com>
2024-04-30 22:26:59 -0700

    updates ch03 counter

---
+++ code/ch03/counter.py
@@ -0,0 +1,15 @@
+"""
+>>> import collections
+>>> ct = collections.Counter('abracadabra')
+>>> ct
+Counter({'a': 5, 'b': 2, 'r': 2, 'c': 1, 'd': 1})
+>>> ct.update('aaaaazzz')
+>>> ct
+Counter({'a': 10, 'z': 3, 'b': 2, 'r': 2, 'c': 1, 'd': 1})
+>>> ct.most_common(3)
+[('a', 10), ('z', 3), ('b', 2)]
+"""
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
